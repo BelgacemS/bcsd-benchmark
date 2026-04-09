66fd1fbe2467290724b4240597ebbea321ad6aa9
BelgacemS <belgacem.smaali@gmail.com>
2026-04-09 21:04:33 +0200

    nettoyage lib/refuse : garde seulement le modele et le checkpoint

--- lib/refuse/data-processing/refuse/run_ghidra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ff in $(find /path/to/binary/dataset/ -type f);
-do
-java -jar ghidra.jar /tmp GhidraTempProject -import $ff -postScript GhidraExtractFunctionBytes -scriptPath . -analysisTimeoutPerFile 60 -deleteProject
-done
